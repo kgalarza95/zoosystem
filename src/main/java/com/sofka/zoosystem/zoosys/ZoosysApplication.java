6d1ee9c487b37a3ae2ae807af4871f2f35b4cd62
Kevin Jordan Galarza Vera <kevin.galarza@sofka.local>
2024-12-09 17:37:56 -0500

    init del proyecto

---
+++ src/main/java/com/sofka/zoosystem/zoosys/ZoosysApplication.java
@@ -0,0 +1,13 @@
+package com.sofka.zoosystem.zoosys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoosysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZoosysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofka/zoosystem/zoosys/ZoosysApplicationTests.java
@@ -0,0 +1,9 @@
+package com.sofka.zoosystem.zoosys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class ZoosysApplicationTests {
+
+
+}
